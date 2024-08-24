304de6c7dc27bcdb2ee7375469a4776d2db7c2ca
awakelife93 <awakelife93@gmail.com>
2024-08-24 16:57:36 +0900

    refactor: change AuditorAwareImpl if -> when

--- src/main/kotlin/com/example/demo/security/service/impl/AuditorAwareImpl.kt
+++ src/main/kotlin/com/example/demo/security/service/impl/AuditorAwareImpl.kt
@@ -9,11 +9,15 @@ class AuditorAwareImpl : AuditorAware<Long> {
   override fun getCurrentAuditor(): Optional<Long> {
     val authentication = SecurityContextHolder.getContext().authentication
 
-    return if (authentication == null || !authentication.isAuthenticated || authentication.principal == "anonymousUser") {
-      Optional.empty()
-    } else {
-      val userAdapter = authentication.principal as UserAdapter
-      Optional.of(userAdapter.securityUserItem.userId)
+    return when {
+      authentication == null ||
+        !authentication.isAuthenticated ||
+        authentication.principal == "anonymousUser" -> Optional.empty()
+
+      else -> {
+        val userAdapter = authentication.principal as UserAdapter
+        Optional.of(userAdapter.securityUserItem.userId)
+      }
     }
   }
 }
